58334221313089ad24a79322cebe0790639e3932
Jefferson dos Anjos <0rakul0render@gmail.com>
2022-04-07 21:48:55 -0300

    tutorial

--- ElasticsearchAPI.py
+++ ElasticsearchAPI.py
@@ -62,6 +62,45 @@ def readOne(search):
             print("\n %(nome)s \n %(idade)s \n %(sexo)s \n %(hobbies)s \n %(pets)s \n %(nome_pet)s"
                   % hit["_source"])
 
+def Update():
+    search = input("Digite uma palavra para ser buscada: ")
+
+    res = es.search(index="bd-python-elastic", body={
+        "query": {
+            "match": {
+                "nome": "" + search + ""
+            }
+        }
+    })
+
+    for hit in res['hits']['hits']:
+        id = hit["_id"]
+        print("\n ID: %s" % hit["_id"])
+        print("\n%(timestamp)s \n %(nome)s \n %(sexo)s \n %(hobbies)s" % hit["_source"])
+        option = input("deseja atualizar o registro?: (s/n) -> ")
+        if option == "s":
+            new_nome = input("\n Digite o novo nome: ")
+            new_idade = input("\n Digite a idade: ")
+            new_sexo = input("\n Digite o sexo: ")
+            new_hobbies = input("\n Digite os novos hobbies: ")
+            new_pets = input("\n Digite o tiopo de pets: ")
+            new_nome_pet = input("\n Digite o nome do pet: ")
+
+            doc = {
+                "nome": new_nome,
+                "idade": new_idade,
+                "sexo": new_sexo,
+                "hobbies": new_hobbies,
+                "pets": new_pets,
+                "nome_pet": new_nome_pet,
+                "timestamp": datetime.now(),
+            }
+
+            res = es.index(index=nome_banco, id=id, document=doc)
+            print("\n status do documento: " + str(res['result']))
+
+            res = es.get(index=nome_banco, id=id)
+            print("\n\n Resultado da busca: \n" + str(res['_source']))
 
 if __name__ == "__main__":
     readOne("guilhem")
